3204503e21530c28a13a8ca366438b131991a8d1
riturajs19 <rituraj.singh.met23@itbhu.ac.in>
2025-03-28 23:16:16 +0530

    pushdata

---
+++ push_data.py
@@ -0,0 +1,10 @@
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://sriturajsingh745:Rajnandini1906*@rituraj.dg9fuac.mongodb.net/?retryWrites=true&w=majority&appName=Rituraj"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
